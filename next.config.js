1f5cc6a6199694ee4db0ba910493c83348f3a256
whayven <waynefosterjr@hotmail.com>
2022-09-22 15:59:21 -0400

    updated images domain to include s3

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'portfolio-bt.s3.amazonaws.com'],
     formats: ['image/avif', 'image/webp'],
   },
 }
