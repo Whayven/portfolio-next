1e517c7d73c1be44a6dd5291e27fd282b2be11dd
whayven <waynefosterjr@hotmail.com>
2022-09-22 16:07:12 -0400

    fixed eslint errors on landing

--- pages/index.js
+++ pages/index.js
@@ -8,81 +8,78 @@ import Link from "next/link";
 
 
 export default function Home({page}) {
-
-    //TODO: GET URI FROM Process Env
-    const uri = 'http://localhost:1337';
-
-    useEffect(() => {
-        console.log(page)
-        console.log(page?.attributes?.Title)
-    }, [])
-
     return (<Container maxWidth='lg' className={styles.container}>
 
-            <main className={styles.main}>
+        <main className={styles.main}>
 
-                <Image height={400} width={800} layout={'intrinsic'}
-                       src={page?.attributes?.Cover?.data?.attributes?.url}/>
-                <Typography variant='h2' gutterBottom>
-                    {page?.attributes?.Title}
-                </Typography>
-                <Typography variant='subtitle1' textAlign={'center'}>
-                    {page?.attributes?.Description}
-                </Typography>
-                <br/>
+            <Image height={400} width={800} layout={'intrinsic'}
+                   src={page?.attributes?.Cover?.data?.attributes?.url}
+                   alt={page?.attributes?.Cover?.data?.attributes?.caption}
+            />
+            <Typography variant='h2' gutterBottom>
+                {page?.attributes?.Title}
+            </Typography>
+            <Typography variant='subtitle1' textAlign={'center'}>
+                {page?.attributes?.Description}
+            </Typography>
+            <br/>
 
-                <Container className={styles.section}>
-                    <Divider className={styles.divider}>
-                        Certifications
-                    </Divider>
-                    <div>
-                        {page?.attributes?.certifications?.data.map((cert, i) => {
-                            return (<div key={i} className={styles.certification}>
-                                    <Image src={cert?.attributes?.Logo?.data?.attributes?.url} height={100}
-                                           width={100} layout={'fixed'}/>
-                                    <br/>
-                                    <Typography variant='h5'>
-                                        {cert?.attributes?.Name}
-                                    </Typography>
-                                </div>)
-                        })}
-                    </div>
-                </Container>
+            <Container className={styles.section}>
+                <Divider className={styles.divider}>
+                    Certifications
+                </Divider>
+                <div>
+                    {page?.attributes?.certifications?.data.map((cert, i) => {
+                        return (<div key={i} className={styles.certification}>
+                            <Image src={cert?.attributes?.Logo?.data?.attributes?.url}
+                                   alt={cert?.attributes?.Name}
+                                   height={100}
+                                   width={100} layout={'fixed'}/>
+                            <br/>
+                            <Typography variant='h5'>
+                                {cert?.attributes?.Name}
+                            </Typography>
+                        </div>)
+                    })}
+                </div>
+            </Container>
 
-                <Container className={styles.section}>
-                    <Divider className={styles.divider}>
-                        Projects
-                    </Divider>
+            <Container className={styles.section}>
+                <Divider className={styles.divider}>
+                    Projects
+                </Divider>
 
-                    <div>
-                        {page?.attributes?.projects?.data.map((project) => {
-                            return (<div key={project?.Id} className={styles.card}>
-                                <a href={project?.attributes?.Url} target={'_blank'}>
-                                    <Typography variant='h2'>
-                                        {project?.attributes?.Title}
-                                    </Typography>
-                                </a>
-                                <br/>
-                                <Typography variant='body2'>
-                                    {project?.attributes?.Description}
+                <div>
+                    {page?.attributes?.projects?.data.map((project) => {
+                        return (<div key={project?.Id} className={styles.card}>
+                            <a href={project?.attributes?.Url}
+                               target={'_blank'}
+                               rel="noopener noreferrer">
+                                <Typography variant='h2'>
+                                    {project?.attributes?.Title}
                                 </Typography>
-                            </div>)
-                        })}
-                    </div>
-                </Container>
-            </main>
+                            </a>
+                            <br/>
+                            <Typography variant='body2'>
+                                {project?.attributes?.Description}
+                            </Typography>
+                        </div>)
+                    })}
+                </div>
+            </Container>
+        </main>
 
 
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by Next.js
-                </a>
-            </footer>
-        </Container>)
+        <footer className={styles.footer}>
+            <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Powered by Next.js
+            </a>
+        </footer>
+    </Container>)
 }
 
 export async function getStaticProps(context) {
