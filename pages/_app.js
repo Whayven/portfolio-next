935a4a614855a14fe2ad9775bdb62c10bc82c194
whayven <waynefosterjr@hotmail.com>
2022-09-30 09:16:51 -0400

    added parallax to landing img banner

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,24 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Head from "next/head";
 import Nav from '../components/Nav';
-import { StyledEngineProvider} from "@mui/material/styles";
+import {StyledEngineProvider} from "@mui/material/styles";
+import {ParallaxProvider} from "react-scroll-parallax";
 
-function MyApp({ Component, pageProps }) {
-  return (
-      <StyledEngineProvider>
-        <Head>
-          <title>Wayne Foster Jr</title>
-          <meta name="description" content="Wayne Foster Jr - 2022"/>
-          <link rel="icon" href="/favicon.ico"/>
-        </Head>
-        <Nav />
-        <Component {...pageProps} />
-      </StyledEngineProvider>
+function MyApp({Component, pageProps}) {
+    return (
+        <StyledEngineProvider>
+            <ParallaxProvider>
+                <Head>
+                    <title>Wayne Foster Jr</title>
+                    <meta name="description" content="Wayne Foster Jr - 2022"/>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
+                <Nav/>
+                <Component {...pageProps} />
+            </ParallaxProvider>
+        </StyledEngineProvider>
 
-  )
+    )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,27 @@ import styles from '../styles/Home.module.css'
 import client from '../util/apolloClient';
 import {GET_LANDING} from '../graphql/pages/queries';
 import Image from "next/image";
+import {Parallax, useParallaxController} from 'react-scroll-parallax';
 
 
 export default function Home({page}) {
+    const uri = 'http://localhost:1337'
+    const parallaxController = useParallaxController();
     return (<Container maxWidth='lg' className={styles.container}>
 
         <main className={styles.main}>
+            <Parallax translateY={[-80, 50]}>
+                <Image height={400} width={800} layout={'intrinsic'}
+                       src={uri + page?.attributes?.Cover?.data?.attributes?.url}
+                       alt={page?.attributes?.Cover?.data?.attributes?.caption}
+                       onLoad={() => parallaxController.update()}
+                />
+            </Parallax>
 
-            <Image height={400} width={800} layout={'intrinsic'}
-                   src={page?.attributes?.Cover?.data?.attributes?.url}
-                   alt={page?.attributes?.Cover?.data?.attributes?.caption}
-            />
-            <Typography variant='h2' textAlign={'center'} gutterBottom>
+            <Typography variant='h2' textAlign={'center'} className={styles.parallax} gutterBottom>
                 {page?.attributes?.Title}
             </Typography>
-            <Typography variant='subtitle1' textAlign={'center'}>
+            <Typography variant='subtitle1' textAlign={'center'} className={styles.parallax}>
                 {page?.attributes?.Description}
             </Typography>
             <br/>
@@ -29,7 +35,7 @@ export default function Home({page}) {
                 <div>
                     {page?.attributes?.certifications?.data.map((cert, i) => {
                         return (<div key={i} className={styles.certification}>
-                            <Image src={cert?.attributes?.Logo?.data?.attributes?.url}
+                            <Image src={uri + cert?.attributes?.Logo?.data?.attributes?.url}
                                    alt={cert?.attributes?.Name}
                                    height={100}
                                    width={100}
