8169d3d7f7240f36844cd031aa5c09f9174ecc45
whayven <waynefosterjr@hotmail.com>
2022-10-12 17:28:41 -0400

    removed img optimization libraries and updated image loader

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     loader:'imgix',
-    path: '/',
+    path: '',
     domains: ['localhost', 'cw-cdk-stack-cloudwaybucketdb51c266-dyy61jgjmzcb.s3.amazonaws.com'],
     formats: ['image/avif', 'image/webp'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
