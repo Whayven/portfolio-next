43ea88b5ace142b8b0f54dc58bce0e003f4746ae
whayven <waynefosterjr@hotmail.com>
2022-10-12 17:28:41 -0400

    removed img optimization libraries and updated image loader

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-const withExportImages = require('next-export-optimize-images');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'portfolio-bt.s3.amazonaws.com'],
+    loader:'imgix',
+    path: '/',
+    domains: ['localhost', 'cw-cdk-stack-cloudwaybucketdb51c266-dyy61jgjmzcb.s3.amazonaws.com'],
     formats: ['image/avif', 'image/webp'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   }
 }
 
-module.exports = withExportImages(nextConfig);
+module.exports = nextConfig;
